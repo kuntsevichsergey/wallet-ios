0a2d79b564d8cd7f43b6cba57498fa34b51cf1b7
sk <kuntsevich.s@gmail.com>
2021-02-14 17:39:50 +0300

    disabling license check

--- build-system/prepare-build.sh
+++ build-system/prepare-build.sh
@@ -24,13 +24,13 @@ esac
 
 BASE_PATH=$(dirname $0)
 
-COPY_PROVISIONING_PROFILES_SCRIPT="$BASE_PATH/copy-provisioning-profiles-$APP_TARGET.sh"
+# COPY_PROVISIONING_PROFILES_SCRIPT="$BASE_PATH/copy-provisioning-profiles-$APP_TARGET.sh"
 PREPARE_BUILD_VARIABLES_SCRIPT="$BASE_PATH/prepare-build-variables-$APP_TARGET.sh"
 
-if [ ! -f "$COPY_PROVISIONING_PROFILES_SCRIPT" ]; then
-	echo "$COPY_PROVISIONING_PROFILES_SCRIPT not found"
-	exit 1
-fi
+# if [ ! -f "$COPY_PROVISIONING_PROFILES_SCRIPT" ]; then
+	# echo "$COPY_PROVISIONING_PROFILES_SCRIPT not found"
+	# exit 1
+# fi
 
 if [ ! -f "$PREPARE_BUILD_VARIABLES_SCRIPT" ]; then
 	echo "$PREPARE_BUILD_VARIABLES_SCRIPT not found"
@@ -42,11 +42,11 @@ rm -rf "$DATA_DIRECTORY"
 mkdir -p "$DATA_DIRECTORY"
 touch "$DATA_DIRECTORY/BUILD"
 
-source "$COPY_PROVISIONING_PROFILES_SCRIPT"
+# source "$COPY_PROVISIONING_PROFILES_SCRIPT"
 source "$PREPARE_BUILD_VARIABLES_SCRIPT"
 
-echo "Copying provisioning profiles..."
-copy_provisioning_profiles "$PROFILES_TYPE"
+# echo "Copying provisioning profiles..."
+# copy_provisioning_profiles "$PROFILES_TYPE"
 
 echo "Preparing build variables..."
 prepare_build_variables "$BUILD_TYPE"
